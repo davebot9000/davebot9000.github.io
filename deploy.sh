7d587c6a7a98500c7c515839f418a6fd798a50f0
David Reese <reesedp@missouri.edu>
2021-09-21 16:03:17 -0500

    Clean up deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,16 @@ set -e
 # build
 pnpm run build
 
-# navigate into the build output directory
-cd src/.vuepress/dist
+# build fresh docs
+rm -rf ./docs && mkdir ./docs
+cp -RP ./src/.vuepress/dist/* ./docs
 
 # if you are deploying to a custom domain
-echo 'davebot9000.github.io' > CNAME
+# echo 'davebot9000.github.io' > CNAME
 
-git add -A
+git add ./docs
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push main
+git push
 # git push -f git@github.com:davebot9000/davebot9000.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
